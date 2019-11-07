bc8315227a20e9d2c28ef610e74c072c340fca7c
Keeling <adam.keeling@austin.utexas.edu>
2019-11-06 22:12:30 -0600

    completed

--- Belly_button_diversity/static/js/app.js
+++ Belly_button_diversity/static/js/app.js
@@ -3,27 +3,69 @@ function buildMetadata(sample) {
   // @TODO: Complete the following function that builds the metadata panel
 
   // Use `d3.json` to fetch the metadata for a sample
-    // Use d3 to select the panel with id of `#sample-metadata`
+  let urlmeta = `/metadata/${sample}`;
+
+  // Use d3 to select the panel with id of `#sample-metadata`
+  d3.json(urlmeta).then(sample => {
+    
+  
+    let metadata_selector = d3.select("#sample-metadata");
 
     // Use `.html("") to clear any existing metadata
+    metadata_selector.html("");
 
     // Use `Object.entries` to add each key and value pair to the panel
     // Hint: Inside the loop, you will need to use d3 to append new
     // tags for each key-value in the metadata.
-
+    Object.entries(sample).forEach(([key,value]) => {
+      metadata_selector.append("p")
+        .text(`${key}: ${value}`)
+        .append("hr");
+    });
+  });
     // BONUS: Build the Gauge Chart
     // buildGauge(data.WFREQ);
-}
+};
 
 function buildCharts(sample) {
 
   // @TODO: Use `d3.json` to fetch the sample data for the plots
-
+  let urlsample = `/samples/${sample}`
     // @TODO: Build a Bubble Chart using the sample data
+
+  d3.json(urlsample).then(function(data) {
+    let trace = {
+      x: data.otu_ids,
+      y: data.sample_values,
+      mode: "markers",
+      marker: {
+        size: data.sample_values,
+        color: data.otu_ids
+      },
+      text: data.otu_labels,
+    }
+
+    let bubbledata = [trace];
+    let bubblelayout = {
+      title: "Bacteria",
+      xaxis: {title: "OTU ID"}
+    }
+    Plotly.newPlot("bubble",bubbledata,bubblelayout);
 
     // @TODO: Build a Pie Chart
     // HINT: You will need to use slice() to grab the top 10 sample_values,
     // otu_ids, and labels (10 each).
+    let pie_data = [{
+      values: data.sample_values.slice(0,10),
+      labels: data.otu_ids.slice(0,10),
+      hovertext: data.otu_labels.slice(0,10),
+      type: "pie"
+    }]
+    let pielayout = {
+      title: "Pies"
+    }
+    Plotly.newPlot("pie",pie_data,pielayout);
+  })
 }
 
 function init() {
